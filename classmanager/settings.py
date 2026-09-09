bd4082959a8a940ab8133916fe766e10fd98ab1c
chiporiromunashe-star <chiporiromunashe@gmail.com>
2026-09-09 10:22:54 +0200

    Define STATIC_ROOT in settings.py

--- classmanager/settings.py
+++ classmanager/settings.py
@@ -126,7 +126,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+STATIC_ROOT =BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
