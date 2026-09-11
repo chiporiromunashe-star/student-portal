3c05a2db9e1823ff8cf40704841dd68bf26ed113
chiporiromunashe-star <chiporiromunashe@gmail.com>
2026-09-12 00:00:55 +0200

    Update views.py

--- classroom/views.py
+++ classroom/views.py
@@ -91,7 +91,10 @@ def user_login(request):
         if user:
             if user.is_active:
                 login(request,user)
-                return HttpResponseRedirect(reverse('home'))
+              if getattr(user, 'is_student', False):
+                 return redirect('classroom:student_dashboard')
+              return HttpResponseRedirect(reverse('home'))
+
 
             else:
                 return HttpResponse("Account not active")
