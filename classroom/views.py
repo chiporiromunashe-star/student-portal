eb57a6020443592d329f94b3d24a2f67c71bdce8
chiporiromunashe-star <chiporiromunashe@gmail.com>
2026-09-11 23:29:27 +0200

    Update views.py

--- classroom/views.py
+++ classroom/views.py
@@ -452,3 +452,7 @@ def change_password(request):
         args = {'form':form}
         return render(request,'classroom/change_password.html',args)
 
+
+@login_required
+def student_dashboard(request):
+    return render(request, 'classroom/student_dashboard.html')
