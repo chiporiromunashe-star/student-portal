38df20ff66780abdd53507d28db010fe31f4ca09
chiporiromunashe-star <chiporiromunashe@gmail.com>
2026-09-12 06:20:53 +0200

    Update urls.py

--- classroom/urls.py
+++ classroom/urls.py
@@ -38,4 +38,6 @@ urlpatterns = [
     path('dashboard/student/', views.student_dashboard, name='student_dashboard'),
     path('subjects/', views.subject_list, name='subject_list'),
     path('dashboard/teacher/', views.teacher_dashboard, name='teacher_dashboard'),
+    path('teacher/subjects/', views.update_teacher_subjects, name='teacher_subjects'),
+    path('student/subjects/', views.update_student_subjects, name='student_subjects'),
 ]
